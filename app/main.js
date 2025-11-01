6bfed55b10158eba6cb9f33d36bc2b792d6369d9
Tanmay Sahu <sahutanmay1903@gmail.com>
2025-11-01 16:44:52 +0530

    Added in loop

--- app/main.js
+++ app/main.js
@@ -6,7 +6,12 @@ const rl = readline.createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`)
-  rl.close();
-});
+
+const promt = () => {
+  rl.question("$ ", (answer) => {
+    console.log(`${answer}: command not found`)
+    promt();
+  });
+}
+
+promt();
